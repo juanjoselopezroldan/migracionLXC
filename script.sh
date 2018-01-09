e769a9d838e97d9fafa527203d65c8367f81e66e
juan jose lopez roldan <jjlr1994@gmail.com>
2018-01-09 18:45:00 +0100

    primeros retoques

--- script.sh
+++ script.sh
@@ -5,5 +5,9 @@ estadomaq1=$(lxc-ls -f | grep debian1 | tr -s " " | cut -d " " -f 2)
 if [[ estadomaq1 == 'RUNNING' ]]; then
 
 else
-  
+  lxc-start -n debian1
+  sleep 2
+
+  ip1=$(lxc-ls --fancy | tr -s " " | cut -d " " -f 5 |  head -2 | tail -1)
+
 fi
