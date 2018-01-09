beed50d85fc116e4b4c0c65dfea67cfda2c11608
juan jose lopez roldan <jjlr1994@gmail.com>
2018-01-09 20:24:52 +0100

    control de primera/segunda maquina

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ echo "Iniciando"
 estadocont1=$(lxc-ls -f | grep debian1 | tr -s " " | cut -d " " -f 2)
 estadocont2=$(lxc-ls -f | grep debian2 | tr -s " " | cut -d " " -f 2)
 
-if [[ estadocont1 == 'RUNNING' and estadocont2 == 'STOPPED' ]]; then
+if [[ estadocont1 == "RUNNING" and estadocont2 == "STOPPED" ]]; then
   memoria1=$(lxc-info -n debian1 | grep 'Memory use' | tr -s " " | cut -d " " -f 3)
   if [[ $memoria1 -ge '470.00' ]]; then
     ip1=$(lxc-ls --fancy | tr -s " " | cut -d " " -f 5 |  head -2 | tail -1)
@@ -20,7 +20,7 @@ if [[ estadocont1 == 'RUNNING' and estadocont2 == 'STOPPED' ]]; then
     mount /dev/disco/lv1 /mnt/debian2/var/www/html/
 
   fi
-elif [[ estadocont1 == 'STOPPED' and estadocont2 == 'STOPPED' ]]; then
+elif [[ estadocont1 == "STOPPED" and estadocont2 == "STOPPED" ]]; then
   echo "Contenedor 1 inactivo, levantando..."
   lxc-start -n debian1
   echo "Montando volumen y obteniendo IP para regla de IPTABLES"
@@ -32,7 +32,7 @@ elif [[ estadocont1 == 'STOPPED' and estadocont2 == 'STOPPED' ]]; then
   echo "Contenedor 1 Operativo"
 fi
 
-#if [[ estadocont2 == 'RUNNING']]; then
+#if [[ estadocont2 == "RUNNING"]]; then
 #  memoria2=$(lxc-info -n debian2 | grep 'Memory use' | tr -s " " | cut -d " " -f 3)
 #  if [[ $memoria2 -ge '980.00' ]]; then
     #statements
