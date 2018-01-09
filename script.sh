89c30fdc3e76ba3bff0b19481ea3e1d37b974ac4
juan jose lopez roldan <jjlr1994@gmail.com>
2018-01-09 19:35:27 +0100

    control de primera maquina

--- script.sh
+++ script.sh
@@ -6,7 +6,15 @@ if [[ estadomaq1 == 'RUNNING' and estadomaq2 == 'STOPPED' ]]; then
   memoria1=$(lxc-info -n debian1 | grep 'Memory use' | tr -s " " | cut -d " " -f 3)
   if [[ $memoria1 -ge '470.00' ]]; then
     ip1=$(lxc-ls --fancy | tr -s " " | cut -d " " -f 5 |  head -2 | tail -1)
-    
+    iptables -t nat -D PREROUTING $ip1
+
+    umount /mnt/debian1/var/www/html/
+
+    lxc-start -n debian2
+
+    ip2=$(lxc-ls --fancy | tr -s " " | cut -d " " -f 5 | tail -1)
+    iptables -I FORWARD -d $ip2/32 -p tcp --dport 80 -j ACCEPT
+    iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $ip2:80
 
 
   fi
