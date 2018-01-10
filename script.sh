d6101025bdb058930358628e05599ebd23a895a1
juan jose lopez roldan <jjlr1994@gmail.com>
2018-01-10 18:06:54 +0100

    comprobación de ram de contenedor 1

--- script.sh
+++ script.sh
@@ -2,46 +2,47 @@
 echo "Iniciando"
 
 estadocont1=$(lxc-ls -f | grep debian1 | tr -s " " | cut -d " " -f 2)
-
+bucle="si"
 estadocont2=$(lxc-ls -f | grep debian2 | tr -s " " | cut -d " " -f 2)
-
-if [[ $estadocont1 == "RUNNING" ]];
-then
-  memoria1=$(lxc-info -n debian1 | grep 'Memory use' | tr -s " " | cut -d " " -f 3 | cut -d "." -f 1)
-  if [[ $memoria1 -ge "470" ]];
+while [[ bucle == "si" ]]; do
+  if [[ $estadocont1 == "RUNNING" ]];
   then
-    echo "Desaciendo iptables y apagando contenedor 1"
-    ip1=$(lxc-ls --fancy | tr -s " " | cut -d " " -f 5 |  head -2 | tail -1)
-    delip=$(iptables -t nat -L --line-number | egrep $ip1 | cut -d " " -f 1)
-    iptables -t nat -D PREROUTING $delip
+    memoria1=$(lxc-info -n debian1 | grep 'Memory use' | tr -s " " | cut -d " " -f 3 | cut -d "." -f 1)
+    if [[ $memoria1 -ge "470" ]];
+    then
+      echo "Desaciendo iptables y apagando contenedor 1"
+      ip1=$(lxc-ls --fancy | tr -s " " | cut -d " " -f 5 |  head -2 | tail -1)
+      delip=$(iptables -t nat -L --line-number | egrep $ip1 | cut -d " " -f 1)
+      iptables -t nat -D PREROUTING $delip
 
-    umount /mnt/debian1/var/www/html/
-    lxc-stop -n debian1
-    echo "Iniciando contenedor 2 y obteniendo ip para configuración de iptables"
-    lxc-start -n debian2
-    sleep 2
-    ip2=$(lxc-ls --fancy | tr -s " " | cut -d " " -f 5 | tail -1)
-    iptables -I FORWARD -d $ip2/32 -p tcp --dport 80 -j ACCEPT
-    iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $ip2:80
-    echo "Montando volumen"
-    mount /dev/disco/lv1 /mnt/debian2/var/www/html/
-    echo "Contenedor 2 Operativo"
+      umount /mnt/debian1/var/www/html/
+      lxc-stop -n debian1
+      echo "Iniciando contenedor 2 y obteniendo ip para configuración de iptables"
+      lxc-start -n debian2
+      sleep 2
+      ip2=$(lxc-ls --fancy | tr -s " " | cut -d " " -f 5 | tail -1)
+      iptables -I FORWARD -d $ip2/32 -p tcp --dport 80 -j ACCEPT
+      iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $ip2:80
+      echo "Montando volumen"
+      mount /dev/disco/lv1 /mnt/debian2/var/www/html/
+      echo "Contenedor 2 Operativo"
+      bucle="no"
+    fi
+  else
+    if [[ $estadocont2 == "STOPPED" ]];
+    then
+      echo "Contenedor 1 inactivo, levantando..."
+      lxc-start -n debian1
+      echo "Montando volumen y obteniendo IP para regla de IPTABLES"
+      sleep 2
+      mount /dev/disco/lv1 /mnt/debian1/var/www/html/
+      ip1=$(lxc-ls --fancy | tr -s " " | cut -d " " -f 5 |  head -2 | tail -1)
+      iptables -I FORWARD -d $ip1/32 -p tcp --dport 80 -j ACCEPT
+      iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $ip1:80
+      echo "Contenedor 1 Operativo"
+    fi
   fi
-else
-  if [[ $estadocont2 == "STOPPED" ]];
-  then
-    echo "Contenedor 1 inactivo, levantando..."
-    lxc-start -n debian1
-    echo "Montando volumen y obteniendo IP para regla de IPTABLES"
-    sleep 2
-    mount /dev/disco/lv1 /mnt/debian1/var/www/html/
-    ip1=$(lxc-ls --fancy | tr -s " " | cut -d " " -f 5 |  head -2 | tail -1)
-    iptables -I FORWARD -d $ip1/32 -p tcp --dport 80 -j ACCEPT
-    iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $ip1:80
-    echo "Contenedor 1 Operativo"
-  fi
-fi
-
+done
 
 if [[ estadocont2 == "RUNNING" ]];
 then
