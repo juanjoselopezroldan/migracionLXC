9c3275b7850d5617248522bee414eda59b18847a
juan jose lopez roldan <jjlr1994@gmail.com>
2018-01-19 12:20:15 +0100

    utlimos retoques

--- script.sh
+++ script.sh
@@ -16,7 +16,8 @@ while [[ $bucle == "si" ]]; do
       ip1=$(lxc-ls --fancy | tr -s " " | cut -d " " -f 5 |  head -2 | tail -1)
       delip=$(iptables -t nat -L --line-number | egrep $ip1 | cut -d " " -f 1)
       iptables -t nat -D PREROUTING $delip
-      umount /mnt/debian1/var/www/html/
+      lxc-attach -n debian1 -- umount /dev/mapper/disco-lv1 /var/www/html
+      lxc-device -n debian1 del /dev/mapper/disco-lv1
       lxc-stop -n debian1
 
       echo "Iniciando contenedor 2 y obteniendo ip para configuración de iptables"
@@ -24,10 +25,11 @@ while [[ $bucle == "si" ]]; do
       sleep 2
 
       ip2=$(lxc-ls --fancy | tr -s " " | cut -d " " -f 5 | tail -1)
-      iptables -I FORWARD -d $ip2/32 -p tcp --dport 80 -j ACCEPT
       iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $ip2:80
       echo "Montando volumen"
-      mount /dev/disco/lv1 /mnt/debian2/var/www/html/
+      lxc-device -n debian2 add /dev/mapper/disco-lv1
+      lxc-attach -n debian2 -- mount /dev/mapper/disco-lv1 /var/www/html
+      lxc-attach -n debian2 -- systemctl restart apache2
 
       echo "Contenedor 2 Operativo"
       bucle="no"
@@ -37,10 +39,11 @@ while [[ $bucle == "si" ]]; do
     lxc-start -n debian1
     echo "Montando volumen y obteniendo IP para regla de IPTABLES"
     sleep 2
-    mount /dev/disco/lv1 /mnt/debian1/var/www/html/
+    lxc-device -n debian1 add /dev/mapper/disco-lv1
+    lxc-attach -n debian1 -- mount /dev/mapper/disco-lv1 /var/www/html
+    lxc-attach -n debian1 -- systemctl restart apache2
 
     ip1=$(lxc-ls --fancy | tr -s " " | cut -d " " -f 5 |  head -2 | tail -1)
-    iptables -I FORWARD -d $ip1/32 -p tcp --dport 80 -j ACCEPT
     iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $ip1:80
     echo "Contenedor 1 Operativo"
   fi
